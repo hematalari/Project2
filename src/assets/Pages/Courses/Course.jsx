148cc7b3a1b856197a1c6e5e236b79a8839eefcd
hematalari <hema.talari08@gmail.com>
2024-06-29 19:27:51 +0530

    update

--- src/assets/Pages/Courses/Course.jsx
+++ src/assets/Pages/Courses/Course.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Course.css'
-import { TbCertificate } from "react-icons/tb";
 import courses_info from '../../Data/courses.js';
 import Navbar from '../../Components/Navbar/Navbar.jsx';
 import Courses from '../../Components/Courses/Courses.jsx';
