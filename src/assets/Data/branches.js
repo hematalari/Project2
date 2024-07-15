b6ca6e2553a2cd69b854622852d8a77aa0c81fa2
hematalari <hema.talari08@gmail.com>
2024-07-15 12:26:12 +0530

    images paths

--- src/assets/Data/branches.js
+++ src/assets/Data/branches.js
@@ -1,31 +1,31 @@
 const courses_info = [
     {
-        src:"./assets/images/Accounts.png",
+        src:"src/assets/images/Accounts.png",
         name:"ACCOUNTS",
         cert:"Certification"
     },
     {
-        src:"./assets/images/Beautican.png",
+        src:"src/assets/images/Beautican.png",
         name:"BEAUTICAN",
         cert:"Certification"
     },
     {
-        src:"./assets/images/Spoken English.png",
+        src:"src/assets/images/Spoken English.png",
         name:"SPOKEN ENGLISH",
         cert:"Certification"
     },
     {
-        src:"./assets/images/MS Office.png",
+        src:"src/assets/images/MS Office.png",
         name:"MS OFFICE",
         cert:"Certification"
     },
     {
-        src:"./assets/images/Tailoring.png",
+        src:"src/assets/images/Tailoring.png",
         name:"TAILORING",
         cert:"Certification"
     },
     {
-        src:"./assets/images/tally.png",
+        src:"src/assets/images/tally.png",
         name:"TALLY",
         cert:"Certification"
     }
--- src/assets/Data/courses.js
+++ src/assets/Data/courses.js
@@ -1,21 +1,21 @@
 const courses_info = [
     {
-        src:"./assets/images/Accounts.png",
+        src:"src/assets/images/Accounts.png",
         name:"ACCOUNTS",
         cert:"Certification"
     },
     {
-        src:"./assets/images/Beautican.png",
+        src:"src/assets/images/Beautican.png",
         name:"BEAUTICAN",
         cert:"Certification"
     },
     {
-        src:"./assets/images/Spoken English.png",
+        src:"src/assets/images/Spoken English.png",
         name:"SPOKEN ENGLISH",
         cert:"Certification"
     },
     {
-        src:"./assets/images/MS Office.png",
+        src:"src/assets/images/MS Office.png",
         name:"MS OFFICE",
         cert:"Certification"
     },
@@ -25,12 +25,12 @@ const courses_info = [
     //     cert:"Certification"
     // },
     {
-        src:"./assets/images/Tailoring.png",
+        src:"src/assets/images/Tailoring.png",
         name:"TAILORING",
         cert:"Certification"
     },
     {
-        src:"./assets/images/tally.png",
+        src:"src/assets/images/tally.png",
         name:"TALLY",
         cert:"Certification"
     }
