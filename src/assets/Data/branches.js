cd723b7327f5e102c9b4bc31f980ce4533d9e53c
hematalari <hema.talari08@gmail.com>
2024-07-15 14:42:52 +0530

    images path changes

--- src/assets/Data/branches.js
+++ src/assets/Data/branches.js
@@ -1,31 +1,31 @@
 const courses_info = [
     {
-        src:"Project2/assets/Images/Accounts.png",
+        src:"/Project2/assets/Images/Accounts.png",
         name:"ACCOUNTS",
         cert:"Certification"
     },
     {
-        src:"Project2/assets/Images/Beautican.png",
+        src:"/Project2/assets/Images/Beautican.png",
         name:"BEAUTICAN",
         cert:"Certification"
     },
     {
-        src:"Project2/assets/Images/Spoken English.png",
+        src:"/Project2/assets/Images/Spoken English.png",
         name:"SPOKEN ENGLISH",
         cert:"Certification"
     },
     {
-        src:"Project2/assets/Images/MS Office.png",
+        src:"/Project2/assets/Images/MS Office.png",
         name:"MS OFFICE",
         cert:"Certification"
     },
     {
-        src:"Project2/assets/Images/Tailoring.png",
+        src:"/Project2/assets/Images/Tailoring.png",
         name:"TAILORING",
         cert:"Certification"
     },
     {
-        src:"Project2/assets/Images/tally.png",
+        src:"/Project2/assets/Images/tally.png",
         name:"TALLY",
         cert:"Certification"
     }
--- src/assets/Data/courses.js
+++ src/assets/Data/courses.js
@@ -1,21 +1,21 @@
 const courses_info = [
     {
-        src:"Project2/assets/Images/Accounts.png",
+        src:"/Project2/assets/Images/Accounts.png",
         name:"ACCOUNTS",
         cert:"Certification"
     },
     {
-        src:"Project2/assets/Images/Beautican.png",
+        src:"/Project2/assets/Images/Beautican.png",
         name:"BEAUTICAN",
         cert:"Certification"
     },
     {
-        src:"Project2/assets/Images/Spoken English.png",
+        src:"/Project2/assets/Images/Spoken English.png",
         name:"SPOKEN ENGLISH",
         cert:"Certification"
     },
     {
-        src:"Project2/assets/Images/MS Office.png",
+        src:"/Project2/assets/Images/MS Office.png",
         name:"MS OFFICE",
         cert:"Certification"
     },
@@ -25,12 +25,12 @@ const courses_info = [
     //     cert:"Certification"
     // },
     {
-        src:"Project2/assets/Images/Tailoring.png",
+        src:"/Project2/assets/Images/Tailoring.png",
         name:"TAILORING",
         cert:"Certification"
     },
     {
-        src:"Project2/assets/Images/tally.png",
+        src:"/Project2/assets/Images/tally.png",
         name:"TALLY",
         cert:"Certification"
     }
